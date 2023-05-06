55ba1281afc042b86f5ca09ce1d410e28cb3eba6
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2023-05-06 16:47:41 +0100

    attempt to preload image

--- components/HeroImage/index.js
+++ components/HeroImage/index.js
@@ -1,6 +1,19 @@
 import { Box } from '@chakra-ui/react'
 import heroImg from '../../images/coral_matt.jpg'
 import Logo from '../Logo'
+import Image from 'next/image'
+import Head from 'next/head'
+import styled from '@emotion/styled'
+
+const CoverImage = styled(Image)`
+  position: absolute;
+  z-index: -1;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`
 
 const HeroImage = () => {
   return (
@@ -9,13 +22,16 @@ const HeroImage = () => {
       display="flex"
       width="100%" 
       height="75vh" 
-      backgroundImage={`linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url(${heroImg.src})`}
+      backgroundImage="linear-gradient(0deg, rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4))"
       backgroundPosition="center"
       backgroundSize="cover"
       backgroundAttachment="fixed"
       justifyContent="center"
       alignItems="center"
+      position="relative"
+      overflow="hidden"
     >
+      <CoverImage src={heroImg} alt="Coral & Matt" priority />
       <Logo color="white" />
     </Box>
   )
