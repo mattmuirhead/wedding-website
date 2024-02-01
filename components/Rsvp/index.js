0bcb2c88626ad448a66b1b543dd06f9b9a26c047
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2024-02-01 19:40:19 +0000

    hopefully netlify actually picks this up

--- components/Rsvp/index.js
+++ components/Rsvp/index.js
@@ -21,65 +21,86 @@ const StyledBox = styled(Box)`
   position: relative;
 `
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 const Rsvp = () => {
   const [name, setName] = useState();
   const [attending, setAttending] = useState();
   const [dietaryReqs, setDietaryReqs] = useState();
+  const [submitted, setSubmitted] = useState(false);
 
   const isAttending = attending === 'true';
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    const myForm = event.target;
-    const formData = new FormData(myForm);
+    const formData = {
+      name,
+      attending,
+      dietaryReqs,
+    }
     
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+      body: encode({ "form-name": "rsvp", ...formData }),
     })
-      .then(() => console.log("Form successfully submitted"))
+      .then(() => setSubmitted(true))
       .catch((error) => alert(error));
   }
 
   return (
   <StyledBox id="rsvp" my={10} p={10}>
     <Container>
-      <Heading size="lg" mb={4}>You in?</Heading>
+      {submitted ? (
+        <>Submitted</>
+      ) : (
+        <>
+          <Heading size="lg" mb={4}>You in?</Heading>
+          <form 
+            method="POST"
+            name="rsvp"
+            data-netlify="true"
+            onSubmit={handleSubmit}
+          >
+            <input type="hidden" name="form-name" value="rsvp" />
+            <FormControl as="fieldset" mb={4}>
+              <Stack direction={['column', 'row']} spacing={[0, 2]}>
+                <FormLabel as="legend">
+                  NAME(S):
+                </FormLabel>
+                <Input variant="flushed" size="sm" name="names" value={name} onChange={e => setName(e.target.value)} />
+              </Stack>
+            </FormControl>
 
-      <form name="rsvp" netlify onSubmit={handleSubmit}>
-        <FormControl as="fieldset" mb={4}>
-          <Stack direction={['column', 'row']} spacing={[0, 2]}>
-            <FormLabel as="legend">
-              NAME(S):
-            </FormLabel>
-            <Input variant="flushed" size="sm" name="names" value={name} onChange={e => setName(e.target.value)} />
-          </Stack>
-        </FormControl>
+            <FormControl as="fieldset" my={10} display="flex" justifyContent={['flex-start', 'center']}>
+              <RadioGroup name="attending" value={attending} onChange={setAttending}>
+                <Stack direction={['column', 'row']} spacing={[2, 6]}>
+                  <Radio value="true">WE&apos;RE IN!</Radio>
+                  <Radio value="false">WE CAN&apos;T MAKE IT</Radio>
+                </Stack>
+              </RadioGroup>
+            </FormControl>
 
-        <FormControl as="fieldset" my={10} display="flex" justifyContent={['flex-start', 'center']}>
-          <RadioGroup name="attending" value={attending} onChange={setAttending}>
-            <Stack direction={['column', 'row']} spacing={[2, 6]}>
-              <Radio value="true">WE&apos;RE IN!</Radio>
-              <Radio value="false">WE CAN&apos;T MAKE IT</Radio>
-            </Stack>
-          </RadioGroup>
-        </FormControl>
+            {isAttending ? (
+              <FormControl as="fieldset" mb={8}>
+                <FormLabel as="legend" textAlign={['left', 'center']}>
+                  LET US KNOW IF YOU HAVE ANY DIETARY REQUIREMENTS!
+                </FormLabel>
+                <Textarea variant="flushed" rows={1} name="dietaryReqs" value={dietaryReqs} onChange={e => setDietaryReqs(e.target.value)} />
+              </FormControl>
+            ) : undefined}
 
-        {isAttending ? (
-          <FormControl as="fieldset" mb={8}>
-            <FormLabel as="legend" textAlign={['left', 'center']}>
-              LET US KNOW IF YOU HAVE ANY DIETARY REQUIREMENTS!
-            </FormLabel>
-            <Textarea variant="flushed" rows={1} name="dietaryReqs" value={dietaryReqs} onChange={e => setDietaryReqs(e.target.value)} />
-          </FormControl>
-        ) : undefined}
-
-        <Box display="flex" justifyContent="center">
-          {!!attending && <Button type="submit" variant="outline">{isAttending ? 'Confirm' : 'Confirm to sacrificing your friendship with us'}</Button>}
-        </Box>
-      </form>
+            <Box display="flex" justifyContent="center">
+              {!!attending && <Button type="submit" variant="outline">{isAttending ? 'Confirm' : 'Confirm to sacrificing your friendship with us'}</Button>}
+            </Box>
+          </form>
+        </>
+      )}
     </Container>
   </StyledBox>
 )}
