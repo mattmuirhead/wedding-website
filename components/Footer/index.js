14cb40e1dd4e49179766a1bb8ed2547595a492f4
Matt Muirhead <mat.rob.muirhead@googlemail.com>
2024-02-05 20:11:59 +0000

    countdown

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,10 +1,7 @@
-import { Box, Container } from '@chakra-ui/react'
+import { Box, Container, Text } from '@chakra-ui/react'
 import logo from '../../images/small-logo.svg'
 import styled from '@emotion/styled'
-
-const Mask = styled.svg`
-  fill: var(--secondary);
-`
+import Countdown from 'react-countdown';
 
 const Wrapper = styled(Box)`
   background-color: var(--secondary);
@@ -12,8 +9,32 @@ const Wrapper = styled(Box)`
 
 const Footer = () => (
   <Wrapper>
-    <Container py={10} maxWidth={150}>
-      <img src={logo.src} alt="Coral and Matt" />
+    <Container py={10} display="flex" justifyContent="center" flexDirection="column" gap={6}>
+      <Box maxWidth={150} m="auto">
+        <img src={logo.src} alt="Coral and Matt" />
+      </Box>
+
+      <Box color="white" textAlign="center">
+        <Countdown
+          date="2024-09-28T00:00:00"
+          intervalDelay={0}
+          precision={3}
+          renderer={({ days, hours, minutes, seconds, completed }) => (
+            <Text size="lg" mb={4}>
+              {completed ? (
+                <>Today&apos;s the day!!!!</>
+              ) : (
+                <>
+                {days} {days === 1 ? 'day' : 'days'},&nbsp;
+                {hours} {hours === 1 ? 'hour' : 'hours'},&nbsp;
+                {minutes} {minutes === 1 ? 'minute' : 'minutes'} and&nbsp;
+                {seconds} {seconds === 1 ? 'second' : 'seconds'}
+                </>
+              )}
+            </Text>
+          )}
+        />
+      </Box>
     </Container>
   </Wrapper>
 )
